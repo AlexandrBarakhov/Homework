fa6e270081784594bbbb1850dbade3904954a511
AlexandrBarakhov <123105865+AlexandrBarakhov@users.noreply.github.com>
2024-04-26 20:53:50 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input("Введите строку с произвольным текстом: ")
+print("Длина введенной строки", len(my_string), "символов")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
